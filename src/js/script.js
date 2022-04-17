f244130699f8828ce1c8da9728ce92a9a214be22
Oleksa <oleksa.antsupov@gmail.com>
2022-04-17 21:04:24 +0300

    add change color for side panel after scroll, edit content, other fixes

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,20 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+$(document).ready(function(){
+    $(window).scroll(function(){  
+        if ($(window).scrollTop() > 700 ){
+            $(".sidepanel__link").css("color", "rgb(6, 35, 4)");
+			$(".sidepanel__divider").css("background-color", "rgb(6, 35, 4)");
+			$(".sidepanel__text").css("color", "rgb(6, 35, 4)");
+		}  else {
+			$(".sidepanel__link").css("color", "white");
+			$(".sidepanel__divider").css("background-color", "white");
+			$(".sidepanel__text").css("color", "white");
+		} 
+    });
+}); 
+
 const counters = document.querySelectorAll('.skills__ratings-counter'),
       lines = document.querySelectorAll('.skills__ratings-line span');
 
