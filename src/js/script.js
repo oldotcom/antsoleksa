e24877eb9154953683cb1af16482b013c70b9235
Oleksa <oleksa.antsupov@gmail.com>
2023-07-30 12:11:31 +0300

    handle form submission with AJAX request

--- src/js/script.js
+++ src/js/script.js
@@ -54,4 +54,19 @@ $(document).ready(function(){
             $('.pageup').fadeOut();
         }
     });
-}); 
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
